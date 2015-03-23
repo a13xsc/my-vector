e8b2ee43883c203e9d8548058ced518fbcea23c7
AxelSC <alex.sever@yahoo.com>
2015-03-23 20:47:09 +0200

    Adaugat redimensionare, assignment operator si ostream in MyVector - 4 teste trecute

--- MyVector.h
+++ MyVector.h
@@ -1,21 +1,34 @@
 #include<iostream>
+using namespace std;
 template <typename T>
 class MyVector{
-	T* p;
+	T* v;
 	int size,capacity;
+	
+	void reducesize(){
+		T* aux;
+		aux=v;
+		v=new T[capacity/2];
+		for(int i=0;i<size;i++){
+			v[i]=aux[i];
+		}
+		delete[] aux;
+		capacity/=2;
+	}
+	
 public:
 	MyVector(){
-		p =new T[10];
+		v =new T[10];
 		size=0;
 		capacity=10;
 	}
 	MyVector(int k){
-		p =new T[k];
+		v =new T[k];
 		size=0;
 		capacity=k;
 	}
 	~MyVector(){
-		delete[] p;
+		delete[] v;
 	}
 	int getSize(){
 		return size;
@@ -23,27 +36,65 @@ public:
 	int getCapacity(){
 		return capacity;
 	}
-	void add(T x){//De adaugat resize
+	void add(T x){//add
 		if(size==capacity){
 			T* aux;
-			aux=p;
-			p=new T[2*capacity];
+			aux=v;
+			v=new T[2*capacity];
 			for(int i=0;i<size;i++){
-				p[i]=aux[i];
+				v[i]=aux[i];
 			}
+			delete[] aux;
 			capacity*=2;
 		}
-		p[size]=x;
+		v[size]=x;
 		size++;
 	}
 	//int operator[](int i) const{return p[i];}
 	T & operator[](int i){
-		return p[i];
+		return v[i];
 	}
-	/*void remove(int i){
-		
+	void remove(int x){//remove
+		if(x<size){
+			for(int i=x;i<size-1;i++){
+				v[i]=v[i+1];
+			}
+			size--;
+			if(size<capacity/2){
+				reducesize();
+			}
+		}
 	}
-	void removeElement(T i){
-		
-	}*/
+	void removeElement(T x){//removeElement
+		for(int i=0;i<size;i++){
+			if(v[i]==x){
+				for(int j=i;j<size-1;j++){
+					v[j]=v[j+1];
+				}
+				size--;
+				break;
+			}
+		}
+		if(size<capacity/2){
+			reducesize();
+		}
+	}
+	void operator=(MyVector& vect){//assignment operator
+		size=vect.size;
+		capacity=vect.capacity;
+		delete[] v;
+		v=new T[size];
+		for(int i=0;i<vect.getSize();i++){
+			v[i]=vect[i];
+		}	
+	}
+	friend std::ostream& operator<<(std::ostream& out,const MyVector<T>& vect);//output
 };
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, MyVector<T>& vect){//output
+		for(int i=0;i<vect.getSize();i++){
+			out<<vect[i]<<" ";
+		}
+		return out;
+	}
